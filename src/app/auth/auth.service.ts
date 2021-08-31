facfebfbdae9c5c19668114c0d27d016d64b6cf9
ManuDev03 <manuprajan03@gmail.com>
2021-08-31 18:22:54 +0530

    sending the signup request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 interface AuthResponseData{
   idToken : string
@@ -16,8 +17,8 @@ export class AuthService {
 
   constructor(private _http:HttpClient) { }
 
-  signup(email:string, password:string){
-    this._http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4cEzpl-HnmLiJgAy8pAVxw869vWBWl7E",{
+  signup(email:string, password:string):Observable<any>{
+    return this._http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4cEzpl-HnmLiJgAy8pAVxw869vWBWl7E",{
       email : email,
       password : password,
       returnSecureToken : true
