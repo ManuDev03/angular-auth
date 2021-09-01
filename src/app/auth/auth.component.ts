b9f5a847d850d8bfed8b057595831e0bbd69da1e
ManuDev03 <manuprajan03@gmail.com>
2021-09-01 17:27:01 +0530

    creating & storing user data

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms'
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
+import { User } from './user.model'
+
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +11,7 @@ import { AuthResponseData, AuthService } from './auth.service';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  user = new Subject<User>()
   isLoginMode = true
   isLoading = false
   error:string = null
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
+import { User} from './user.model'
 
 export interface AuthResponseData {
   idToken: string;
@@ -16,6 +17,7 @@ export interface AuthResponseData {
   providedIn: 'root',
 })
 export class AuthService {
+  user: any;
   constructor(private _http: HttpClient) {}
 
   signup(email: string, password: string): Observable<any> {
@@ -28,7 +30,9 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError))
+      .pipe(catchError(this.handleError),tap(resData =>{
+        this.handleAuthentication(resData.email,resData.localId,resData.idToken, +resData.expiresIn)
+      }))
   }
 
   login(email: string, password: string) {
@@ -39,7 +43,21 @@ export class AuthService {
         password: password,
         returnSecureToken: true,
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError),tap(resData =>{
+      this.handleAuthentication(resData.email,resData.localId,resData.idToken, +resData.expiresIn)
+    }));
+  }
+
+  private handleAuthentication(
+    email:string,
+    userId:string,
+    token:string,
+    expiresIn:number
+  ){
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
+    const user = new User(email,userId,token,expirationDate)
+    this.user.next(user)
+
   }
 
   private handleError(errorRes: HttpErrorResponse){
